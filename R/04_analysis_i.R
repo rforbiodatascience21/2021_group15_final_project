4d3fbfc6ec8f040fa0fba1942b466d59f1e9a25f
ehedeboe <s211253@student.dtu.dk>
2021-04-26 11:17:44 +0000

    Made survival plot

--- R/04_analysis_i.R
+++ R/04_analysis_i.R
@@ -54,11 +54,23 @@ pbc_data_aug %>%
   theme_classic()
 
 # survival time
+
+# get row names ordered by number of follow up dayes
+cumulative_count <- pbc_data_aug %>% 
+  filter(status == 1) %>% 
+  arrange(fu.days) %>% 
+  rownames() %>% 
+  as.numeric()
+
+# plot survival percentage over follow up time
 pbc_data_aug %>% 
-  group_by(status) %>% 
+  filter(status == 1) %>% 
+  arrange(fu.days) %>%
+  ggplot(mapping = aes(x= fu.days)) + 
+  geom_step(aes(x=fu.days,y=(312-cumulative_count)/312)) 
   
 
-
+pbc_data_aug %>% rownames() %>% as.numeric()
 
 library(ggcorrplot)
 
@@ -75,6 +87,6 @@ pbc_data_aug %>% select_if(is.numeric) %>%
         axis.text.x = element_text(size = 8)) +
   labs(title = "Correlation matrix") 
 
-)
+
 
 
