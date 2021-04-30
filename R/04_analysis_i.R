d7667664bc6b46c5a2e2e61209ac1533966eca6b
MichellePihl <s174347@student.dtu.dk>
2021-04-30 13:17:42 +0000

    All plots in 04_analysis_i.R have titles and legends now.

--- R/04_analysis_i.R
+++ R/04_analysis_i.R
@@ -5,6 +5,7 @@ rm(list = ls())
 # Load libraries ----------------------------------------------------------
 library(tidyverse)
 library(ggplot2)
+library(patchwork)
 
 # Load data ---------------------------------------------------------------
 pbc_data_aug <- read_csv("data/03_pbc_data_aug.csv")
@@ -25,72 +26,141 @@ pbc_data_aug <- pbc_data_aug %>%
 drug_boxplot <- pbc_data_aug %>% 
   ggplot(mapping = aes(x = age, y = drug, fill = sex)) +
   geom_boxplot(alpha = 0.5) +
-  theme_classic()
+  theme_classic() + 
+  labs(title = "Distribution of age in the two drug treatments for both sex",
+       x = "Age", y = "Drug",
+       caption = "Data from https://hbiostat.org/data/") +
+  theme(plot.caption = element_text(hjust = 1, face = "italic"),
+        plot.title.position = "plot", plot.caption.position = "plot")
 
 pbc_data_aug %>% 
   group_by(mayo_risk_level) %>% 
   ggplot(mapping = aes(x = age, y = mayo_risk_level, fill = sex)) +
   geom_boxplot(alpha = 0.5) +
-  theme_classic()
+  theme_classic() +
+  labs(title = "Distribution of age in the three Mayo risk score levels for both sex,",
+       x = "Age", y = "Mayo risk score level",
+       caption = "Data from https://hbiostat.org/data/") +
+  theme(plot.caption = element_text(hjust = 1, face = "italic"),
+        plot.title.position = "plot", plot.caption.position = "plot") +
+  scale_fill_discrete(name = "Sex")
 
 
-# % in each may risk column
+# % in each mayo risk column
 pbc_data_aug %>% 
   count(mayo_risk_level) %>% 
   mutate(pct = n/ sum(n) * 100) %>% 
   ggplot(aes(x = mayo_risk_level, y = pct)) +
-  geom_bar(stat = "identity")
+  geom_bar(stat = "identity", alpha = 0.5, aes(fill = mayo_risk_level)) +
+  theme_classic() +
+  labs(title = "Distribution of participants in the three Mayo risk score levels",
+       x = "Mayo risk score level", y = "Percentage",
+       caption = "Data from https://hbiostat.org/data/") +
+  theme(plot.caption = element_text(hjust = 1, face = "italic"),
+        plot.title.position = "plot", plot.caption.position = "plot", legend.position = "none") +
+  scale_fill_discrete(limits = rev)
 
 # % of each sex
 pbc_data_aug %>% 
   count(sex) %>% 
   mutate(pct = n/ sum(n) * 100) %>% 
   ggplot(aes(x = sex, y = pct)) +
-  geom_col()
+  geom_bar(stat = "identity", alpha = 0.5, aes(fill = sex)) +
+  theme_classic() +
+  labs(title = "Distribution of sex in the data",
+       x = "Sex", y = "Percentage",
+       caption = "Data from https://hbiostat.org/data/") +
+  theme(plot.caption = element_text(hjust = 1, face = "italic"),
+        plot.title.position = "plot", plot.caption.position = "plot", legend.position = "none")
 
 # bilirubin and fu days
 pbc_data_aug %>%
   ggplot(mapping = aes(
     x = fu.days,
     y = bili,
-    colour = mayo_risk_level,
+    color = mayo_risk_level,
     shape = sex
   )) +
-  geom_point() +
-  scale_shape_manual(values=c(3, 19))
+  geom_point() +  
+  theme_classic() +
+  labs(title = "Correlation between Serum Bilirubin and Time to Death or Liver Transplantation",
+       x = "Time to Death or Liver Transplantation", y = "Serum Bilirubin (mg/dl)",
+       caption = "Data from https://hbiostat.org/data/") +
+  theme(plot.caption = element_text(hjust = 1, face = "italic"),
+        plot.title.position = "plot", plot.caption.position = "plot") +
+  scale_shape_manual(values=c(3, 19), name = "Sex") +
+  scale_color_discrete(name = "Mayo risk score level", limits = rev)
 
 
 # follow up days histogram
-pbc_data_aug %>% 
+p1 <- pbc_data_aug %>% 
   ggplot(mapping = aes(x = fu.days)) +
-  geom_histogram(binwidth = 365.25) +
-  theme_classic()
+  geom_histogram(binwidth = 365.25, fill= "royalblue4", color = "royalblue4", alpha = 0.5) +
+  theme_classic() +
+  labs(title = "Time to Death or Liver Transplantation",
+       x = "Time to Death or Liver Transplantation", y = "Count") +
+  theme(plot.caption = element_text(hjust = 1, face = "italic"),
+        plot.title.position = "plot", plot.caption.position = "plot", legend.position = "none")
 
-pbc_data_aug %>% 
+
+p2 <- pbc_data_aug %>% 
   filter(status == 1) %>% 
   ggplot(mapping = aes(x = fu.days)) +
-  geom_histogram(binwidth = 365.25) +
-  theme_classic()
+  geom_histogram(binwidth = 365.25, aes(fill= "red", color = "red"), alpha = 0.5) +
+  theme_classic() +
+  labs(title = "Participants with status 1",
+       x = "Time to Death or Liver Transplantation", y = "Count") +
+  theme(plot.caption = element_text(hjust = 1, face = "italic"),
+        plot.title.position = "plot", plot.caption.position = "plot", legend.position = "none")
 
-pbc_data_aug %>% 
+p3 <- pbc_data_aug %>% 
   filter(status == 0) %>% 
   ggplot(mapping = aes(x = fu.days)) +
-  geom_histogram(binwidth = 365.25) +
-  theme_classic()
+  geom_histogram(binwidth = 365.25, fill= "green4", color = "green4", alpha = 0.5) +
+  theme_classic() +
+  labs(title = "Participants with status 0",
+       x = "Time to Death or Liver Transplantation", y = "Count",
+       caption = "Data from https://hbiostat.org/data/") +
+  theme(plot.caption = element_text(hjust = 1, face = "italic"),
+        plot.title.position = "plot", plot.caption.position = "plot", legend.position = "none")
 
-pbc_data_aug %>% 
+# Plots of fu.days
+p1 / (p2 + p3)
+
+p1 <- pbc_data_aug %>% 
   mutate(status = factor(status, levels = c(0,1))) %>% 
   ggplot(mapping = aes(x = fu.days, y = status)) +
-  geom_boxplot(alpha = 0.5) +
-  theme_classic()
+  geom_boxplot(alpha = 0.5, fill = "royalblue4", color = "royalblue4") +
+  theme_classic() +
+  labs(title = "Time to Death or Liver Transplantation in relation to status",
+       x = "Time to Death or Liver Transplantation", y = "Status") +
+  theme(plot.caption = element_text(hjust = 1, face = "italic"),
+        plot.title.position = "plot", plot.caption.position = "plot", legend.position = "none")
 
-
-pbc_data_aug %>%
+p2 <- pbc_data_aug %>%
   filter(status == 1) %>% 
   mutate(stage = factor(stage, levels = c(1,2,3,4))) %>% 
   ggplot(mapping = aes(x = fu.days, y = stage)) +
-  geom_boxplot(alpha = 0.5) +
-  theme_classic()
+  geom_boxplot(alpha = 0.5, fill = "red", color = "red") +
+  theme_classic() +
+  labs(title = "Participants in status 1 in stage 1-4",
+       x = "Time to Death or Liver Transplantation", y = "Stage") +
+  theme(plot.caption = element_text(hjust = 1, face = "italic"),
+        plot.title.position = "plot", plot.caption.position = "plot", legend.position = "none")
+
+p3 <- pbc_data_aug %>%
+  filter(status == 0) %>% 
+  mutate(stage = factor(stage, levels = c(1,2,3,4))) %>% 
+  ggplot(mapping = aes(x = fu.days, y = stage)) +
+  geom_boxplot(alpha = 0.5, fill = "green4", color = "green4") +
+  theme_classic() +
+  labs(title = "Participants in status 0 in stage 1-4",
+       x = "Time to Death or Liver Transplantation", y = "Stage", 
+       caption = "Data from https://hbiostat.org/data/") +
+  theme(plot.caption = element_text(hjust = 1, face = "italic"),
+        plot.title.position = "plot", plot.caption.position = "plot", legend.position = "none")
+
+p1 / (p2 + p3)
 
 
 pbc_data_aug %>%
@@ -98,36 +168,64 @@ pbc_data_aug %>%
   mutate(stage = factor(stage, levels = c(1,2,3,4))) %>% 
   ggplot(mapping = aes(x = fu.days, y = stage, fill = drug)) +
   geom_boxplot(alpha = 0.5) +
-  theme_classic()
+  theme_classic() +
+  labs(title = "Time to Death or Liver Transplantation at stage 1 to 4 in relation to the two drugs",
+       x = "Time to Death or Liver Transplantation", y = "Stage", 
+       caption = "Data from https://hbiostat.org/data/") +
+  theme(plot.caption = element_text(hjust = 1, face = "italic"),
+        plot.title.position = "plot", plot.caption.position = "plot") +
+  scale_fill_discrete(name = "Drug")
 
 
 pbc_data_aug %>%
   mutate(status = factor(status, levels = c(0,1))) %>% 
   ggplot(mapping = aes(x = fu.days, y = status, fill = drug)) +
   geom_boxplot(alpha = 0.5) +
-  theme_classic()
-
-
+  theme_classic() +
+  labs(title = "Time to Death or Liver Transplantation at status 0 or 1 in relation to the two drugs",
+       x = "Time to Death or Liver Transplantation", y = "Status", 
+       caption = "Data from https://hbiostat.org/data/") +
+  theme(plot.caption = element_text(hjust = 1, face = "italic"),
+        plot.title.position = "plot", plot.caption.position = "plot") +
+  scale_fill_discrete(name = "Drug")
 
 
 pbc_data_aug %>% 
   mutate(status = factor(status, levels = c(0,1))) %>%
   ggplot(aes(x = status, fill = drug)) +
-  geom_bar(position = "dodge")
+  geom_bar(position = "dodge") +
+  theme_classic() +
+  labs(title = "Number of participants in status 0 or 1 in relation to the two drugs",
+       x = "Status", y = "Count", 
+       caption = "Data from https://hbiostat.org/data/") +
+  theme(plot.caption = element_text(hjust = 1, face = "italic"),
+        plot.title.position = "plot", plot.caption.position = "plot") +
+  scale_fill_discrete(name = "Drug")
 
 
 
 pbc_data_aug %>% 
   ggplot(mapping = aes(x = platelet, y = protime, color = stage)) +
-  geom_point()
+  geom_point() +
+  theme_classic() +
+  labs(title = "Correlation between Platelets and Prothrombin Time at stages 1 to 4",
+       x = "Platelets (per cm^3/1000)", y = "Prothrombin Time (sec.)", 
+       caption = "Data from https://hbiostat.org/data/") +
+  theme(plot.caption = element_text(hjust = 1, face = "italic"),
+        plot.title.position = "plot", plot.caption.position = "plot") +
+  scale_fill_discrete(name = "Stage")
 
 
 pbc_data_aug %>% 
   ggplot(mapping = aes(x = spiders, fill = status)) +
-  geom_bar(position = "dodge")
-  
-
-
+  geom_bar(position = "dodge")  +
+  theme_classic() +
+  labs(title = "Count of participants with and without spiders",
+       x = "Spiders", y = "Count", 
+       caption = "Data from https://hbiostat.org/data/") +
+  theme(plot.caption = element_text(hjust = 1, face = "italic"),
+        plot.title.position = "plot", plot.caption.position = "plot") +
+  scale_fill_discrete(name = "Stage")
 
 
 # survival time
@@ -144,7 +242,14 @@ pbc_data_aug %>%
   filter(status == 1) %>% 
   arrange(fu.days) %>%
   ggplot(mapping = aes(x= fu.days, color = drug)) + 
-  geom_step(aes(x=fu.days,y=(312-cumulative_count)/312)) 
+  geom_step(aes(x=fu.days,y=(312-cumulative_count)/312))  +
+  theme_classic() +
+  labs(title = "Time to Death or Liver Transplantation in relation to the drugs",
+       x = "Time to Death or Liver Transplantation", y = "Cumulative count", 
+       caption = "Data from https://hbiostat.org/data/") +
+  theme(plot.caption = element_text(hjust = 1, face = "italic"),
+        plot.title.position = "plot", plot.caption.position = "plot") +
+  scale_color_discrete(name = "Drug") 
 
 
 pbc_data_aug %>%
@@ -152,7 +257,14 @@ pbc_data_aug %>%
   filter(status == 1) %>% 
   arrange(fu.days) %>%
   ggplot(mapping = aes(x= fu.days, color = stage)) + 
-  geom_step(aes(x=fu.days,y=(312-cumulative_count)/312))
+  geom_step(aes(x=fu.days,y=(312-cumulative_count)/312)) +
+  theme_classic() +
+  labs(title = "Time to Death or Liver Transplantation in relation to the 4 stages",
+       x = "Time to Death or Liver Transplantation", y = "Cumulative count", 
+       caption = "Data from https://hbiostat.org/data/") +
+  theme(plot.caption = element_text(hjust = 1, face = "italic"),
+        plot.title.position = "plot", plot.caption.position = "plot") +
+  scale_color_discrete(name = "Stage") 
 
 pbc_data_aug %>%
   group_by(status) %>% 
@@ -169,10 +281,15 @@ pbc_data_aug %>% select_if(is.numeric) %>%
              lab_size = 3) +
   theme(legend.title=element_text(size=8), 
         legend.text=element_text(size=8),
-        title = element_text(size=10),
-        axis.text.y = element_text(size = 8),
-        axis.text.x = element_text(size = 8)) +
-  labs(title = "Correlation matrix") 
+        title = element_text(size=10)) +
+  theme_classic() +
+  labs(title = "Correlation matrix") +
+  theme(axis.text.y = element_text(size = 8),
+        axis.text.x = element_text(size = 8, angle = 45, 
+                                   vjust = 1, hjust = 1)) +
+  labs(caption = "Data from https://hbiostat.org/data/") +
+  theme(plot.caption = element_text(hjust = 1, face = "italic"),
+        plot.title.position = "plot", plot.caption.position = "plot")
 
 
 
