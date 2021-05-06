723674e662e0ff4584f1bd28872d3674ad40ede1
ehedeboe <s211253@student.dtu.dk>
2021-05-06 07:35:01 +0000

    Added documents to doit

--- R/00_doit.R
+++ R/00_doit.R
@@ -4,6 +4,8 @@ source(file = "R/02_clean.R")
 source(file = "R/03_augment.R")
 source(file = "R/04_analysis_i.R")
 source(file = "R/05_analysis_ii.R")
+source(file = "R/06_clusteing.R")
+source(file = "R/07_PCA")
 
 
 # Knit the Presentation slides --------------------------------------------
