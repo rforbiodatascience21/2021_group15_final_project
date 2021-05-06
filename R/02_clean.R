0997b318eb1a576229d28b37608fc70a5f3242e2
ehedeboe <s211253@student.dtu.dk>
2021-05-06 07:35:27 +0000

    Saved number of NA's as tables

--- R/02_clean.R
+++ R/02_clean.R
@@ -4,34 +4,38 @@ rm(list = ls())
 
 # Load libraries ----------------------------------------------------------
 library(tidyverse)
+library(kableExtra)
+library(gt)
 
 # Load data ---------------------------------------------------------------
 pbc_data <- read_csv("data/01_pbc_data.csv")
 
 
+# Look at missing values --------------------------------------------------
+na_table <- pbc_data %>%
+  summarise_all(list(~ sum(is.na(.))))
+
 # Remove non-trial subjects -----------------------------------------------
 pbc_data <- pbc_data %>% 
   filter(drug != "not randomized")
 
-
 # Impute missing values ---------------------------------------------------
 
-# how many observations have missing values?
-pbc_data %>%
-  select_if(is.numeric) %>% summarise_all(list(~ sum(is.na(.))))
+# How many observations have missing values now?
+na_table2 <- pbc_data %>%
+  summarise_all(list(~ sum(is.na(.))))
 
-# mutate missing values
+# Mutate missing values by assigning the mean
 pbc_data <- pbc_data %>%
   mutate_all(list(~ ifelse(is.na(.), mean(., na.rm = TRUE),.)))
 
-
 # Round off variables -----------------------------------------------------
 
-# round down age to whole years
-#pbc_data <- pbc_data %>% 
-#  mutate(age = floor(age))
+# Round down age to whole years
+pbc_data <- pbc_data %>% 
+  mutate(age = floor(age))
 
-# round off numeric columns
+# Round off numeric columns
 pbc_data <- pbc_data %>% 
   mutate_if(is.numeric, round, digits = 2)
 
