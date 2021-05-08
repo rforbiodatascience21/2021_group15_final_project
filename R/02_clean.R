d013f10b2f38747b185db8c0c6838dd70b6e5a00
ehedeboe <s211253@student.dtu.dk>
2021-05-08 20:11:19 +0000

    Moved table saving to bottom

--- R/02_clean.R
+++ R/02_clean.R
@@ -16,18 +16,21 @@ pbc_data <- read_csv("data/01_pbc_data.csv")
 na_table1 <- pbc_data %>%
   summarise_all(list(~ sum(is.na(.))))
 
+
 # Remove non-trial subjects -----------------------------------------------
 pbc_data <- pbc_data %>% 
   filter(drug != "not randomized")
 
 
 # Missing values after removal of non-trial subjects ----------------------
+
 # How many observations have missing values now?
 na_table2 <- pbc_data %>%
   summarise_all(list(~ sum(is.na(.))))
 
 
 # Table of missing values in data -----------------------------------------
+
 # Create a long table of the  missing values
 na_table1_long <- na_table1 %>%  
   pivot_longer(all_of(colnames(.)),
@@ -42,21 +45,13 @@ na_table2_long <-
 # Joing the two tibbles
 na_values <- full_join(na_table1_long,na_table2_long,by="Variable")
 
-# Create a table
-table1 <- na_values %>% 
-  gt() %>% 
-  tab_header(
-  title = md("Table of missing values in the data"))
-  
-
-# Save in results
-table1 %>% 
-  gtsave(filename = "results/table1.png")
 
 # Impute missing values ---------------------------------------------------
+
 # Mutate missing values by assigning the mean
 pbc_data <- pbc_data %>%
   mutate_all(list(~ ifelse(is.na(.), mean(., na.rm = TRUE), .)))
+
 
 # Round off variables -----------------------------------------------------
 
@@ -68,11 +63,21 @@ pbc_data <- pbc_data %>%
 pbc_data <- pbc_data %>% 
   mutate_if(is.numeric, round, digits = 2)
 
+
 # Write data --------------------------------------------------------------
 pbc_data_clean <- pbc_data
 
 write_csv(x = pbc_data_clean, file = "data/02_pbc_data_clean.csv")
 
+# Create a table with NA values
+na_table <- na_values %>% 
+  gt() %>% 
+  tab_header(
+    title = md("Table of missing values in the data"))
+
+# Save in results
+na_table %>% 
+  gtsave(filename = "results/na_table.png")
 
 
 
