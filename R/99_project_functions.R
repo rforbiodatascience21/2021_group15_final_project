8b4d0b1e36feb6e5d11b0dc31050613dc77d7c36
LeneChr <s163628@student.dtu.dk>
2021-05-09 07:25:13 +0000

    The last changes

--- R/99_project_functions.R
+++ R/99_project_functions.R
@@ -3,23 +3,15 @@
 # Converting specific columns to factors
 factor_columns <- function(data) {
   data <- data %>% 
-    mutate(sex = factor(sex, levels = c("male", "female")))
-  
-  data <- data %>%
+    mutate(sex = factor(sex, levels = c("male", "female"))) %>%
     mutate_at(., 
               vars(spiders, hepatom, ascites), 
-              list(~ factor(., levels = c("absent", "present"))))
-  
-  data <- data %>%
+              list(~ factor(., levels = c("absent", "present")))) %>%
     mutate(mayo.risk.level = factor(mayo.risk.level,
                                     levels =  c("low risk",
                                                 "medium risk",
-                                                "high risk")))
-  
-  data <- data %>% 
-    mutate(status = factor(status, levels = c(0, 1)))
-  
-  data <- data %>% 
+                                                "high risk"))) %>%
+    mutate(status = factor(status, levels = c(0, 1))) %>% 
     mutate(stage = factor(stage, levels = c(1, 2, 3, 4)))
   return(data)
 }
