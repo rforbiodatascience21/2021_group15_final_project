7f8246541bfdfe99a14abd09200a2c9e3eaf007e
ehedeboe <s211253@student.dtu.dk>
2021-05-10 07:07:13 +0000

    Code formatting

--- R/99_project_functions.R
+++ R/99_project_functions.R
@@ -3,16 +3,25 @@
 # Converting specific columns to factors
 factor_columns <- function(data) {
   data <- data %>% 
-    mutate(sex = factor(sex, levels = c("male", "female"))) %>%
-    mutate_at(., 
-              vars(spiders, hepatom, ascites), 
-              list(~ factor(., levels = c("absent", "present")))) %>%
+    mutate(sex = factor(sex, 
+                        levels = c("male", 
+                                   "female"))) %>%
+    mutate_at(vars(spiders, 
+                   hepatom, 
+                   ascites), 
+              list(~ factor(., 
+                            levels = c("absent", 
+                                       "present")))) %>%
     mutate(mayo.risk.level = factor(mayo.risk.level,
                                     levels =  c("low risk",
                                                 "medium risk",
                                                 "high risk"))) %>%
-    mutate(status = factor(status, levels = c(0, 1))) %>% 
-    mutate(stage = factor(stage, levels = c(1, 2, 3, 4)))
+    mutate(status = factor(status, levels = c(0, 
+                                              1))) %>% 
+    mutate(stage = factor(stage, levels = c(1, 
+                                            2, 
+                                            3, 
+                                            4)))
   return(data)
 }
 
